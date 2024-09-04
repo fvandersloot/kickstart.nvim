ca77c7f8d5f5b1f08e0ae250856b3a4c0cce6a8b
Floris van der Sloot <floris@vandersloot.xyz>
2024-09-04 11:17:38 +0200

    fixed module problem

--- init.lua
+++ init.lua
@@ -3,10 +3,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+require 'config.keymaps'
+require 'config.options'
+require 'config.autocmds'
 
--- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
