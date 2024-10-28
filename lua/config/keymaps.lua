d46cb5e436e099a1dd646b56a74afc02a02312a4
Floris van der Sloot <floris@vandersloot.xyz>
2024-10-28 16:08:28 +0100

    Added keymap for centering

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_keymap('n', 'j', 'jzz', {})
+vim.api.nvim_set_keymap('n', 'k', 'kzz', {})
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', {})
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', {})
